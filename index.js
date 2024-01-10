290d03b429979c12df8b8b7aca38f9caf69fa260
adrianosuprine <adrianosuprine@gmail.com>
2024-01-10 14:45:53 +0300

    modifying elements

--- index.js
+++ index.js
@@ -15,24 +15,23 @@
 // element.append(ul);
 
 const main = document.getElementById("main");
-main.innerHTML =
-  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
-const element = document.getElementById("main");
-element.style.height = "300px";
-element.style.backgroundColor = "#27647B";
-element.textContent = "You've changed what's on the screen!";
-element.style.fontSize = "24px";
-element.style.marginLeft = "30px";
-element.style.lineHeight = 2;
+// main.innerHTML =
+//   "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+// const element = document.getElementById("main");
+// element.style.height = "300px";
+// element.style.backgroundColor = "#27647B";
+// element.textContent = "You've changed what's on the screen!";
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
 // element.remove();
 main.remove();
-// var newHeader = document.createElement("h1");
-// document.body.append(newHeader);
-// newHeader.id = "victory";
-const newHeader = document.querySelector("body");
-newHeader.innerHTML = 
-"<h1 id='victory'> Adriano is the champion </h1>";
-newHeader = document.createElement("h1");
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+// const newHeader = document.querySelector("body");
+// newHeader.innerHTML = 
+// "<h1 id='victory'> Adriano is the champion </h1>";
+// newHeader = document.createElement("h1");
 // document.body.append(newHeader);
 // newHeader.id = "victory";
 
@@ -41,5 +40,5 @@ newHeader = document.createElement("h1");
 // const elementa = document.getElementById("victory");
 
 
-// elementa.textContent = " Adriano is the champion ";
+newHeader.textContent = " Adriano is the champion ";
 
